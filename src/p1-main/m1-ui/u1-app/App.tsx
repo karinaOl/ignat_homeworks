930991f3fe46254cc3a927d510a51dbbd2caf606
karinaOl <karina.olkok@yandex.ru>
2022-04-13 11:47:21 +0300

    homework 7

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import s from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5/HW5";
 import HW6 from "../../../p2-homeworks/h6/HW6";
+import HW7 from "../../../p2-homeworks/h7/HW7";
 
 function App() {
     return (
@@ -9,7 +10,7 @@ function App() {
             <div>react homeworks:</div>
             <HW5/>
             <HW6/>
-
+            <HW7/>
         </div>
     )
 }
