f51841865f2307e0c121bd6c43003d127fd469ca
karinaOl <karina.olkok@yandex.ru>
2022-04-29 11:06:30 +0300

    homework 9

---
+++ src/p2-homeworks/h9/Clock.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+
+function Clock() {
+    const [timerId, setTimerId] = useState<number>(0)
+    const [date, setDate] = useState<Date>(new Date())
+    const [show, setShow] = useState<boolean>(false)
+
+    const stop = () => {
+        clearInterval(timerId)// stop
+    }
+    const start = () => {
+        stop()
+        const id: number = window.setInterval(() => {
+            setDate(new Date())// setDate
+        }, 1000)
+        setTimerId(id)
+    }
+
+    const onMouseEnter = () => {
+        setShow(true)
+    }
+    const onMouseLeave = () => {
+        setShow(false)
+    }
+
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
+
+    return (
+        <div>
+            <div
+                onMouseEnter={onMouseEnter}
+                onMouseLeave={onMouseLeave}
+            >
+                {stringTime}
+            </div>
+
+            {show && (
+                <div>
+                    {stringDate}
+                </div>
+            )}
+
+            <SuperButton onClick={start}>start</SuperButton>
+            <SuperButton onClick={stop}>stop</SuperButton>
+
+        </div>
+    )
+}
+
+export default Clock
